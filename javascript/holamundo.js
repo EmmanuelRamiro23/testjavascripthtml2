c04852d14a63ba46f7d139a8b152030fc9fa7d4e
Luis Emmanuel Ramiro Luna <luisemmanuelpueblagmail.com>
2025-11-04 17:30:12 -0600

    Nuevos datos agregados

---
+++ javascript/holamundo.js
@@ -0,0 +1,9 @@
+window.alert("Hola Mundo de javascript bb");
+//Veamos a hacer una funcion en javascript
+function sumar(c, b) {
+  return c + b;
+}
+window.alert("El resultado de la suma es de " + sumar(5, 3));
+for (let i = 0; i < 5; i++) {
+  window.alert("El valor de i es: " + i);
+}
---
+++ javascript/holamundo2.js
@@ -0,0 +1,4 @@
+console.log("Hola mundo de la programacion en javascript");
+/*Esto nos ayuda a escribir en el documento html en el navegador*/
+document.write("Hola mundo bb Emmanuel");
+//Veamos otro apartado en este lado
